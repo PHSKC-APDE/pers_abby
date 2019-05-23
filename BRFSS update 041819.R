e264a2ed7c459e077611b6df3e515354b6d52de9
Schachter <SCHACA@ph.lcl>
2019-05-23 10:09:06 -0700

    Initial commit of CHI Tableau-ready output code

---
+++ BRFSS update 041819.R
@@ -0,0 +1,169 @@
+options(max.print = 350, tibble.print_max = 30, scipen = 999)
+
+install.packages("tidyverse")
+install.packages("openxlsx")
+library(tidyverse) # Manipulate data
+library(openxlsx) # Read and write Excel files
+
+chi_path <- "//Phshare01/epe_share/WORK/CHI Visualizations/BRFSS Indicators (all)/BRFSS (all)"
+
+brfss <- read.xlsx(file.path(chi_path, "BRFSS_combined_tableau_suppressed.xlsx"), sheet=1)
+
+brfss_path <- "//Phshare01/epe_share/WORK/CHI Visualizations/BRFSS Indicators (all)/BRFSS (all)/BRFSS 2017 indicator updates/NewFormat"
+
+#import new indicator data
+ecig <-  read.xlsx(file.path(brfss_path, "ECIGA_suppress_2019-0516 (new format).xlsm"), sheet="results")
+smoker <- read.xlsx(file.path(brfss_path, "CIGA_suppress_2019-0516 (new format).xlsm"), sheet="results") 
+firearm <- read.xlsx(file.path(brfss_path, "GUNHOME_suppress_2019-0516 (new format).xlsm"), sheet="results") 
+# replace with suppressed version!
+obese <- read.xlsx(file.path(brfss_path, "OBEA_nosuppress_2019-0516 (new format).xlsm"), sheet="results")
+physact <- read.xlsx(file.path(brfss_path, "PHYSAMET_suppress_2019-0516 (new format).xlsm"), sheet="results")
+physact1824 <- read.xlsx(file.path(brfss_path, "PHYSAMET1824_suppress_2019-0516 (new format).xlsm"), sheet="results")
+mjuse <- read.xlsx(file.path(brfss_path, "MJUSE_suppress_2019-0516 (new format).xlsm"), sheet="results")
+
+# combine new indicators into single table
+brfss_new <-bind_rows(ecig, smoker, firearm, obese, physact, physact1824, mjuse) 
+
+# delete old versions of indicators from existing BRFSS table and replace with updated data
+brfss_updated <- brfss %>% 
+  filter(!Indicator %in% c("smoker1","firearm4","obese","_pastaer", "mjpast30")) %>% 
+#update category labels
+    mutate(
+        Tab = case_when(
+        Tab == "Subgroups" ~ "demgroups",
+        Tab == "SmallGroups" ~ "crosstabs",
+        Tab == "_King County" ~ "_kingco",
+        Tab == "Trends" ~ "trends"),
+      Category1 = case_when(
+        Category1 == "Race/Ethnicity" ~ "Race/ethnicity",
+        Category1 == "Health Reporting Areas" ~ "Cities/neighborhoods",
+        Category1 == "Income" ~ "Household income",
+        Category1 == "Household Income" ~ "Household income",
+        Category1 == "King County regions" ~ "Regions",
+        TRUE ~ Category1),
+      Category2 = case_when(
+        Category2 == "Race/Ethnicity" ~ "Race/ethnicity",
+        Category2 == "Health Reporting Areas" ~ "Cities/neighborhoods",
+        Category2 == "King County regions" ~ "Regions",
+        Category2 == "Region" ~ "Regions",
+        Tab == "Subgroups" ~ "Overall",
+        TRUE ~ Category2),
+      Cat1varname = case_when(
+        Cat1varname == 'mrace' ~ "race3",
+        TRUE ~ Cat1varname),
+      Cat2varname = case_when(
+        Cat2varname == 'mrace' ~ "race3",
+        Tab == "Subgroups" ~ "Overall",
+        TRUE ~ Cat2varname),
+      Group = case_when(
+        Group == 'Fed Way-Dash Point/Woodmont' ~ 'Fed Way-Dash Pt',
+        Group == '>=$100,000' ~ '$100,000+',
+        TRUE ~ Group),
+      Subgroup = case_when(
+        Tab == "Subgroups" ~ "Overall",
+        Subgroup == 'Fed Way-Dash Point/Woodmont' ~ 'Fed Way-Dash Pt',
+        Subgroup == '>=$100,000' ~ '$100,000+',
+        TRUE ~ Subgroup),
+      cat1_group_alias = case_when(
+        Group == "Beacon/Gtown/S.Park" ~ "Beacon Hill/Georgetown/South Park",
+        Group == "Bellevue-NE" ~ "Bellevue-Northeast",
+        Group == "Black Diamond/Enumclaw/SE County" ~ "Black Diamond/Enumclaw/Southeast County",
+        Group == 'Capitol Hill/E.lake' ~ "Capitol Hill/Eastlake",
+        Group == "Fed Way-Central/Military Rd" ~ "Federal Way-Central/Military Rd",
+        Group == "Fed Way-Dash Pt" ~ "Federal Way-Dash Point/Woodmont",
+        Group == "Kenmore/LFP" ~ "Kenmore/Lake Forest Park",
+        Group == "Kent-SE" ~ "Kent-Southeast",
+        Group == "Mercer Isle/Pt Cities" ~ "Mercer Island/Point Cities",
+        Group == "NE Seattle" ~ "Northeast Seattle",
+        Group == "NW Seattle" ~ "Northwest Seattle",
+        Group == "SE Seattle" ~ "Southeast Seattle",
+        Group == "QA/Magnolia" ~ "Queen Anne/Magnolia",
+        grepl(' NH', Group) == TRUE ~ sub(' NH', '-NH', Group),
+        TRUE ~ Group),
+      cat2_group_alias = case_when(
+        Subgroup == "Beacon/Gtown/S.Park" ~ "Beacon Hill/Georgetown/South Park",
+        Subgroup == "Bellevue-NE" ~ "Bellevue-Northeast",
+        Subgroup == "Black Diamond/Enumclaw/SE County" ~ "Black Diamond/Enumclaw/Southeast County",
+        Subgroup == 'Capitol Hill/E.lake' ~ "Capitol Hill/Eastlake",
+        Subgroup == "Fed Way-Central/Military Rd" ~ "Federal Way-Central/Military Rd",
+        Subgroup == "Fed Way-Dash Pt" ~ "Federal Way-Dash Point/Woodmont",
+        Subgroup == "Kenmore/LFP" ~ "Kenmore/Lake Forest Park",
+        Subgroup == "Kent-SE" ~ "Kent-Southeast",
+        Subgroup == "Mercer Isle/Pt Cities" ~ "Mercer Island/Point Cities",
+        Subgroup == "NE Seattle" ~ "Northeast Seattle",
+        Subgroup == "NW Seattle" ~ "Northwest Seattle",
+        Subgroup == "SE Seattle" ~ "Southeast Seattle",
+        Subgroup == "QA/Magnolia" ~ "Queen Anne/Magnolia",
+        grepl(" NH", Subgroup) == TRUE ~ sub(' NH', '-NH', Subgroup),
+        TRUE ~ Subgroup),
+      caution = ifelse(rse>30, "!", ""),
+      Comparisonwith.KC = ifelse(Comparisonwith.KC == 'nodiff', 'no different', Comparisonwith.KC),
+      source_date = "") %>% 
+rename("indicator_key" ="Indicator", 
+        "tab" = "Tab",
+        "year" = "Year",
+        "cat1" = "Category1",
+        "cat1_group" = "Group",
+        "cat1_varname" = "Cat1varname",
+        "cat2" = "Category2",
+        "cat2_group" = "Subgroup",
+        "cat2_varname" = "Cat2varname",
+        "result" = "Percent",
+        "lower_bound" = "Lower.bound",
+        "upper_bound" = "Upper.bound",
+        "comparison_with_kc" = "Comparisonwith.KC",
+        "time_trends" = "Timetrends",
+        "significance" = "Significance.level",
+        "denominator" = "Sample_size",
+        "suppression" = "Suppress",
+        "run_date" = "runid") %>% 
+  select(data_source, indicator_key, tab, year, cat1, cat1_group, cat1_group_alias, cat1_varname, cat2, cat2_group, cat2_group_alias, cat2_varname, result, lower_bound, upper_bound, se, rse, comparison_with_kc, time_trends, significance, caution, everything()) %>% 
+  bind_rows(., brfss_new) 
+  
+# metadata
+brfss_metadata <- read.xlsx(file.path(chi_path, "BRFSS_combined_tableau_suppressed.xlsx"), sheet=2)
+
+ecig_meta <-  read.xlsx(file.path(brfss_path, "ECIGA_suppress_2019-0516 (new format).xlsm"), sheet="metadata")
+smoker_meta <- read.xlsx(file.path(brfss_path, "CIGA_suppress.xlsm"), sheet="metadata")
+firearm_meta <- read.xlsx(file.path(brfss_path, "GUNHOME_suppress.xlsm"), sheet="metadata") 
+obese_meta <- read.xlsx(file.path(brfss_path, "OBEA_suppress.xlsm"), sheet="metadata") 
+physact_meta <- read.xlsx(file.path(brfss_path, "PHYSAMET_suppress.xlsm"), sheet="metadata") 
+physact1824_meta <- read.xlsx(file.path(brfss_path, "PHYSAMET1824_suppress.xlsm"), sheet="metadata") 
+mjuse_meta <- read.xlsx(file.path(brfss_path, "MJUSE_suppress.xlsm"), sheet="metadata")
+
+brfss_meta_new <-bind_rows(ecig_meta, smoker_meta, firearm_meta, obese_meta, physact_meta,physact1824_meta, mjuse_meta) %>% 
+  filter(Item == "title") %>% 
+  mutate(title = case_when(
+          varname == "firearm4" ~ "Firearms Stored in Home",
+          TRUE ~ title),
+         ratetype = 'percent',
+         Indicator = varname, 
+         latest_rate = as.numeric(sub("%","", latest_rate))/100) %>% 
+  select(-Item,-Value) %>% 
+  select(Indicator, title, latest_data, ratetype:trendsummary, varname, everything())
+
+
+brfss_meta_updated <- brfss_metadata %>% 
+  filter(!Indicator %in% c("smoker1","firearm4","obese","_pastaer", "mjpast30")) %>% 
+  bind_rows(., brfss_meta_new) %>% 
+  mutate(valence = case_when(
+           varname == "firearm4" ~ "neutral",
+           varname == "_pastaer_v1" ~ "positive",
+           varname == "_pastaer_v2" ~ "positive",
+           TRUE ~ "negative"),
+         map_type = "HRA",
+         data_source = tolower(data_source),
+         latest_year_kc_pop = NA_integer_,
+         latest_year_count = NA_real_,
+         unit = "adults",
+         chi = ifelse(Indicator == "_pastaer_v2", 0, 1)) %>% 
+  rename("indicator_key" = "Indicator",
+         "short_name" = "title",
+         "result_type" = "ratetype",
+         "latest_year_result" = "latest_rate",
+         "run_date" = "runid") %>% 
+  select(-latest_data, -trendsummary, -varname, -Topic_new, -Domain_new, -savname, -sav_abbrev, -task_id) %>% 
+  select (indicator_key, short_name, result_type, valence, latest_year, latest_year_result, latest_year_kc_pop, latest_year_count, map_type, unit, valid_years, chi, run_date)
+ 
+list_of_datasets <- list("results" = brfss_updated, "metadata" = brfss_meta_updated)
+write.xlsx(list_of_datasets, file = "//Phshare01/epe_share/WORK/CHI Visualizations/BRFSS Indicators (all)/BRFSS (all)/BRFSS_combined_tableau_suppressed_041819.xlsx")
---
+++ Fix up comparison with KC fields.r
@@ -0,0 +1,152 @@
+#########
+# Fix up time trend data in death and birth CHIs
+#
+# Alastair Matheson
+# 2018-10
+#########
+
+#### Set up global parameters and call in libraries ####
+options(max.print = 350, tibble.print_max = 30, scipen = 999)
+
+library(tidyverse) # Manipulate data
+library(openxlsx) # Read and write Excel files
+
+chi_path <- "//Phshare01/epe_share/WORK/CHI Visualizations"
+
+
+#### BRING IN DATA ####
+firearm_death <- read.xlsx(file.path(chi_path, "Death Indicators (all)",
+                              "DEATH_firearm-related_updated 2012-2016.xlsx")) %>% 
+  mutate(Cat1varname = ifelse(Category1 == "King County regions", "ccreg", Cat1varname),
+         Cat1varname = ifelse(grepl("-NH", Group, fixed=TRUE), "race4", 
+                              ifelse(grepl("- NH", Group, fixed=TRUE), "race4", 
+                                     ifelse(Group == "Hispanic as Race", "race4", 
+                                            ifelse(Group == "Hispanic", "race4", 
+                                                   ifelse(Category1 == "Race/Ethnicity", "race3", Cat1varname))))), 
+         Group = recode(Group, "American Indian/Alaskan Native Only-NH" = "AIAN - NH",
+                        "American Indian/Alaskan Native Only" = "AIAN",
+                        "Black Only" = "Black",
+                        "Black Only-NH" = "Black - NH",
+                        "Asian Only" = "Asian",
+                        "Asian Only-NH" = "Asian - NH",
+                        "Hispanic as Race" = "Hispanic",
+                        "Hispanic - any race" = "Hispanic",
+                        "Pacific Islander Only-NH" = "NHPI - NH",
+                        "Pacific Islander Only" = "NHPI",
+                        "White Only-NH" = "White - NH"), 
+         Tab = ifelse(Cat1varname == "race3" & Tab != "Trends" & is.na(Cat2varname), "Subgroups", Tab),
+         Cat1varname = (ifelse(Group=="White - NH", "race4", Cat1varname ))) %>%
+  select(-Comparison_with_KC, -X22, -Significance_level) %>% 
+  filter(!is.na(indicator_key))
+
+
+#birth <- read.xlsx(file.path(chi_path, "Birth Indicators (all)",   "BIRTH_combined_suppress.xlsx"))
+
+#acs <- read.xlsx(file.path(chi_path, "ACS Indicators (all)",   "ACS combined_2016 updated data.xlsx"))
+
+#### DEATH DATA ####
+death_kc <- firearm_death %>%
+  filter(Category1 == "King County" & !is.na(indicator_key)) %>%
+  select(indicator_key, Year, Lower_bound, Upper_bound, Tab) %>%
+  mutate(Tab = ifelse(Tab=="_King County", "Subgroups", Tab),
+         Lower_bound = round(Lower_bound, digits=1),
+         Upper_bound = round(Upper_bound, digits=1)) %>% 
+  rename(lb_kc = Lower_bound, ub_kc = Upper_bound)
+
+death_trend <- firearm_death %>%
+  filter(!is.na(indicator_key)) %>%
+  select(indicator_key, Year, Tab, Category1,Cat1varname, Group, Lower_bound, Upper_bound, Suppress) %>%
+  mutate(Lower_bound = round(Lower_bound, digits=1),
+         Upper_bound = round(Upper_bound, digits=1)) %>% 
+  left_join(., death_kc, by = c("indicator_key", "Year", "Tab")) %>%
+  mutate(Comparison_with_KC = case_when(Suppress == "Y" ~ NA_character_,
+                                        Lower_bound > ub_kc ~ "higher",
+                                        Upper_bound < lb_kc ~ "lower",
+                                        Lower_bound < ub_kc | Upper_bound > lb_kc ~ "no different",
+                                        TRUE ~ NA_character_),
+         Significance_level = ifelse(Comparison_with_KC == "higher", "*", ifelse(Comparison_with_KC == "lower", "*", NA))) %>% 
+  select(indicator_key, Year, Tab, Category1, Cat1varname, Group, Comparison_with_KC, Significance_level)
+
+firearm_death_combined <- left_join(firearm_death, death_trend, by = c("indicator_key", "Year", "Tab", "Category1", "Cat1varname", "Group")) %>%
+    select(data_source:rse, Comparison_with_KC, Significance_level, Timetrends:Suppress)
+
+
+write.xlsx(firearm_death_combined, file = file.path(chi_path, "Death Indicators (all)",
+                                            "DEATH_firearm_related_corrected_KC_comp.xlsx"))
+
+death <- read.xlsx(file.path(chi_path, "Death Indicators (all)",
+                                     "death_combined_suppress_old.xlsx"))
+
+death <- death %>% 
+    filter(indicator_key != "dth801000") %>% 
+    rbind(firearm_death_combined) %>% 
+        mutate (Rate = round(Rate, digits=1),
+           Lower_bound = round(Lower_bound, digits=1),
+           Upper_bound = round(Upper_bound, digits=1))
+
+
+write.xlsx(death, file = file.path(chi_path, "Death Indicators (all)",
+                                                    "death_combined_suppress.xlsx"))
+
+
+# #### BIRTH DATA ####
+# #birth_kc <- birth %>%
+# #  filter(Tab == "Trends" & Category1 == "King County") %>%
+# #  select(indicator_key, Year, Lower_bound, Upper_bound) %>%
+#   rename(lb_kc = Lower_bound, ub_kc = Upper_bound)
+# 
+# birth_trend <- birth %>%
+#   filter(Tab == "Trends") %>%
+#   select(indicator_key, Year, Category1, Group, Lower_bound, Upper_bound, Suppress) %>%
+#   left_join(., birth_kc, by = c("indicator_key", "Year")) %>%
+#   mutate(Comparison_with_KC = case_when(Suppress == "Y" ~ NA_character_,
+#                                         Lower_bound > ub_kc ~ "higher",
+#                                         Upper_bound < lb_kc ~ "lower",
+#                                         Lower_bound < ub_kc | Upper_bound > lb_kc ~ "no different",
+#                                         TRUE ~ NA_character_)) %>%
+#   select(indicator_key, Year, Category1, Group, Comparison_with_KC)
+# 
+# birth_combined <- left_join(birth, birth_trend, by = c("indicator_key", "Year", "Category1", "Group")) %>%
+#   mutate(Comparison_with_KC = ifelse(is.na(Comparison_with_KC.y),
+#                                      Comparison_with_KC.x,
+#                                      Comparison_with_KC.y),
+#          Cat1varname = ifelse(Category1 == "King County regions", "ccreg", Cat1varname)) %>%
+#   select(-Comparison_with_KC.x, -Comparison_with_KC.y) %>%
+#   select(data_source:rse, Comparison_with_KC, Significance_level:Suppress)
+# 
+# 
+# write.xlsx(birth_combined, file = file.path(chi_path, "birth Indicators (all)",
+#                                             "birth_combined_suppress_corrected_KC_comp.xlsx"))
+# 
+# 
+# #### ACS DATA ####
+# acs_kc <- acs %>%
+#   filter(Tab == "Trends" & Group == "King County") %>%
+#   select(indicator, house_type, Year, Lower.bound, Upper.bound) %>%
+#   rename(lb_kc = Lower.bound, ub_kc = Upper.bound)
+# 
+# acs_trend <- acs %>%
+#   filter(Tab == "Trends") %>%
+#   select(indicator, Year, house_type, Category1, Group, Lower.bound, Upper.bound, Suppression.label) %>%
+#   left_join(., acs_kc, by = c("indicator", "Year", "house_type")) %>%
+#   mutate(Comparisonwith.KC = case_when(Suppression.label == "^" ~ NA_character_,
+#                                         Lower.bound > ub_kc ~ "higher",
+#                                         Upper.bound < lb_kc ~ "lower",
+#                                         Lower.bound < ub_kc | Upper.bound > lb_kc ~ "no different",
+#                                         TRUE ~ NA_character_)) %>%
+#   select(indicator, Year, house_type, Category1, Group, Comparisonwith.KC)
+# 
+# acs_combined <- left_join(acs, acs_trend, by = c("indicator", "Year", "house_type", "Category1", "Group")) %>%
+#   mutate(Comparisonwith.KC = ifelse(is.na(Comparisonwith.KC.y),
+#                                      Comparisonwith.KC.x,
+#                                      Comparisonwith.KC.y)) %>%
+#   select(-Comparisonwith.KC.x, -Comparisonwith.KC.y) %>%
+#   select(data_source:rse, Comparisonwith.KC, Trend:Denominator) %>%
+#   mutate_at(vars(Percent, Lower.bound, Upper.bound),
+#             funs(ifelse(Percent > 1, round(., 0), round(., 3)))) %>%
+#   mutate(se = ifelse(Percent > 1, round(se, 0), round(se, 4)),
+#          rse = round(rse, 1))
+# 
+# 
+# write.xlsx(acs_combined, file = file.path(chi_path, "ACS Indicators (all)",
+#                                           "ACS combined_2016 updated data_corrected_KC_comp.xlsx"))
