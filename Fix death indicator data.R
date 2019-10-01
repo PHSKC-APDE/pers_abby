fa91e37f95d7e351d57693491024ef9eb161aad2
Abigail Schachter <38510059+aschachter@users.noreply.github.com>
2019-10-01 11:51:51 -0700

    Fixed race3 / race4 categories in death data

---
+++ Fix death indicator data.R
@@ -0,0 +1,28 @@
+library(odbc)
+
+db_store51 <- dbConnect(odbc(), "PHExtractStore51")
+
+# db_store <- dbConnect(odbc::odbc(),
+#                          .connection_string = "Driver={SQL Server};Server={KCITSQLUTPDBH51};
+#                          Database={PHExtractStore51};Trusted_Connection=True")
+
+
+tbl_id_results<- DBI::Id(schema = "APDE_WIP", table = "death_results")
+tbl_id_meta<-  DBI::Id(schema = "APDE_WIP", table = "death_metadata")
+  
+death_results <- DBI::dbReadTable(db_store51, tbl_id_results)
+death_meta <-DBI::dbReadTable(db_store51, tbl_id_meta)
+death_results <- death_results %>% 
+  mutate(Cat1varname = ifelse(Group=="Hispanic" & indicator_key != 'dth801000', 'race3', Cat1varname)) %>% 
+  select(indicator)
+         
+
+
+dbWriteTable(db_store51, tbl_id_death, death_results, overwrite = T)
+
+
+chi_path<- "S:/WORK/CHI Visualizations/Death Indicators (all)"
+
+dfs <- list("results" = death_results, "metadata" = death_meta)
+
+write.xlsx(dfs, file.path(chi_path, "/DEATH_combined_suppress_updateddth801000.xlsx"))
